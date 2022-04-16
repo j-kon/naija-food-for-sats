a22b2be2df3b2c6958ffd9e046a4b2a53211481c
Jeremiah Jacob <jaykon4me@gmail.com>
2022-04-16 16:53:10 +0100

    starter commit

---
+++ src/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const home = () => {
+    return (
+        <div>
+            <h1 className="text-center mt-3">All item</h1>
+            <section className="py-4 container"></section>
+        </div>
+    );
+};
+export default home;
