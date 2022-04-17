50cdc724127ecc1c9bc4793c576a0fe0de5a428d
Jeremiah Jacob <jaykon4me@gmail.com>
2022-04-17 07:21:55 +0100

    base ui and functionality

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: 'Juicy Grapes',
+            desc: '',
+            price: 45,
+        },
+        {
+            id: 1,
+            img: img2,
+            title: 'Orange',
+            desc: '',
+            price: 43,
+        },
+        {
+            id: 1,
+            img: img3,
+            title: 'Mango',
+            desc: '',
+            price: 55,
+        }
+    ],
+};
+
+export default data;
--- src/home.js
+++ src/home.js
@@ -1,11 +1,29 @@
 import React from "react";
+import Itemcard from "./itemcard";
+import data from "./data";
 
-const home = () => {
+const Home = () => {
     return (
-        <div>
+        <>
             <h1 className="text-center mt-3">All item</h1>
-            <section className="py-4 container"></section>
-        </div>
+            <section className="py-4 container">
+                <div className="row justify-content-center">
+                    {data.productData.map((item, index) => {
+                        return (
+                            <Itemcard
+                                img={item.img}
+                                title={item.title}
+                                desc={item.desc}
+                                price={item.price}
+                                item={item}
+                                key={index}
+                            />
+                        )
+                    })}
+
+
+                </div></section>
+        </>
     );
 };
-export default home;
+export default Home;
