31486b17e38b7b65eba6952561cb909de4725ffd
Jeremiah Jacob <jaykon4me@gmail.com>
2022-04-19 21:38:24 +0100

    install @radar/lnrpc

--- src/Cart.js
+++ src/Cart.js
@@ -1,71 +1,132 @@
 import React from "react";
-import { useCart } from "react-use-cart"
+import { useCart } from "react-use-cart";
 
+function ItemCardModal() {
+  return (
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Modal title
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">...</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 const Cart = () => {
-    const {
-        isEmpty,
-        totalUniqueItems,
-        items,
-        totalItems,
-        cartTotal,
-        updateItemQuantity,
-        removeItem,
-        emptyCart,
-    } = useCart();
-    
-    if (isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>
-    return (
-        <section className="py-4 container">
-            <div className="row justify-content-center">
-                <div className="col-12">
-                    <h3>Cart ({totalUniqueItems}) total items: ({totalItems})</h3>
-                    <table className="table table-light table-hover m-0">
-                        <tbody>
-                            {items.map((item, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>
-                                            <img src={item.img} style={{ height: '6rem' }} alt="product" />
-                                        </td>
-                                        <td>{item.title}</td>
-                                        <td>{item.price}</td>
-                                        <td>Quantity ({item.quantity})</td>
-                                        <td>
-                                            <button
-                                                className="btn btn-info ms-2"
-                                                onClick={() => updateItemQuantity(item?.id, item.quantity - 1)}
-                                            >-</button>
-                                            <button
-                                                className="btn btn-info ms-2"
-                                                onClick={() => updateItemQuantity(item?.id, item.quantity + 1)}
-                                            >+</button>
-                                            <button
-                                                className="btn btn-denger ms-2"
-                                                onClick={() => removeItem(item?.id,)}
-                                            >Remove Item</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
+  const [isModal, setIsModal] = React.useState(false);
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
 
-                </div>
-                <div className="col-auto ms-auto">
-                    <h2>Total Price: $ {cartTotal}</h2>
-                </div>
-                <div className="col-auto">
-                    <button
-                        className="btn btn-danger m-2"
-                        onClick={() => emptyCart()}
-                    >Clear Cart</button>
-                    <button className="btn btn-primary mm-2 ">Buy Now</button>
-
-                </div>
-
-            </div>
-        </section>
-    );
+  if (isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>;
+  return (
+    <>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          <div className="col-12">
+            <h3>
+              Cart ({totalUniqueItems}) total items: ({totalItems})
+            </h3>
+            <table className="table table-light table-hover m-0">
+              <tbody>
+                {items.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>
+                        <img
+                          src={item.img}
+                          style={{ height: "6rem" }}
+                          alt="product"
+                        />
+                      </td>
+                      <td>{item.title}</td>
+                      <td>{item.price}</td>
+                      <td>Quantity ({item.quantity})</td>
+                      <td>
+                        <button
+                          className="btn btn-info ms-2"
+                          onClick={() =>
+                            updateItemQuantity(item?.id, item.quantity - 1)
+                          }
+                        >
+                          -
+                        </button>
+                        <button
+                          className="btn btn-info ms-2"
+                          onClick={() =>
+                            updateItemQuantity(item?.id, item.quantity + 1)
+                          }
+                        >
+                          +
+                        </button>
+                        <button
+                          className="btn btn-denger ms-2"
+                          onClick={() => removeItem(item?.id)}
+                        >
+                          Remove Item
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <div className="col-auto ms-auto">
+            <h2>Total Price: $ {cartTotal}</h2>
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-danger m-2" onClick={() => emptyCart()}>
+              Clear Cart
+            </button>
+            <button
+              className="btn btn-primary mm-2 "
+              onClick={() => setIsModal(true)}
+            >
+              Pay Now
+            </button>
+          </div>
+        </div>
+      </section>
+      {isModal && <ItemCardModal />}
+    </>
+  );
 };
 
 export default Cart;
